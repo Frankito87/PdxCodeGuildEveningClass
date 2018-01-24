3364714c2b9930fc86a495331d0486ae37514514
Frankito87 <frank_ramos_87@yahoo.com>
2018-01-23 20:20:58 -0800

    update

--- python/dec_18_night_class/matlib.py
+++ python/dec_18_night_class/matlib.py
@@ -1,50 +1,30 @@
 import random
 
-name = input("What is your name: ")
 
-#adj = input("Give me a adjective: ")
-adj = input("Give me three adjective separeted by comma: ")
-
-p = adj.split()
-
-print(p)
-
-animal = input("Give me a name of a animal: ")
-
-vocal = input("Give me a vocal: ")
-
-movie = input("Give me a name of a movie: ")
-
-x = " Your name is {} you give a adjective {} you select this animal {} you give me this vocal {} and you type this movie {} ".format(name, random.choice(p), animal, vocal, movie)
-
-print(x)
-
-keep_going = input("Do you want to keep going ! Please select Yes or No !: ")
-
-while keep_going == "Yes":
+def mat_lib_game():
 
     name = input("What is your name: ")
-
-#adj = input("Give me a adjective: ")
+    # adj = input("Give me a adjective: ")
     adj = input("Give me three adjective separeted by comma: ")
-
     p = adj.split()
-
     print(p)
-
     animal = input("Give me a name of a animal: ")
-
     vocal = input("Give me a vocal: ")
-
     movie = input("Give me a name of a movie: ")
+    print("Your name is {} you give a adjective {} you select this animal {} you give me this vocal {} and you type "
+          "this movie {} ".format(name, random.choice(p), animal, vocal, movie))
 
-    x = " Your name is {} you give a adjective {} you select this animal {} you give me this vocal {} and you type this movie {} ".format(name, random.choice(p), animal, vocal, movie)
-
-    print(x)
-
-    # keep_going = keep_going + 1
-    keep_going = input("Do you want to keep going ! Please select Yes or No !: ")
+    play_again()
 
 
-else:
-    print("You finish the game!! Thanks!")
+def play_again():
+
+    keep_going = input("Do you want to keep going ! Please select Yes or No !: ").lower()
+
+    if keep_going == "yes":
+        mat_lib_game()
+    else:
+        quit()
+
+
+mat_lib_game()
