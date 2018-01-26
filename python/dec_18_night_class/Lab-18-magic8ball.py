3284466ae780bbee39838358b3e091a1741714e4
Frankito87 <frank_ramos_87@yahoo.com>
2018-01-25 19:10:58 -0800

    update

--- python/dec_18_night_class/Lab-18-magic8ball.py
+++ python/dec_18_night_class/Lab-18-magic8ball.py
@@ -1,20 +1,21 @@
 import random
 
+
 def magicball():
     string = "Welcome to the Magic 8 Ball game!! "
     print(string.center(50, " "))
 
-    prediction = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
-               'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Do not count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+    prediction = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+                  'As I see it', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
+                  'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
+                  'Do not count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
     answer = random.choice(prediction)
 
     question = input("Enter the question: ")
     print(answer)
 
-
     play_again()
-
 
 
 def play_again():
@@ -25,8 +26,6 @@ def play_again():
     elif a in 'no':
         print("Good Bye!!")
         exit()
-
-
     else:
         print(" I did not understand that! ")
 
